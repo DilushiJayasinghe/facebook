75e1b51dc7fbaef776ce83e4ccd0ea388678e057
DilushiJayasinghe <dilushijayasinghe02@gmail.com>
2024-02-18 11:10:39 +0530

    add css styles

--- facebook-login/src/App.jsx
+++ facebook-login/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Login from "./components/login"
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- facebook-login/src/components/login.jsx
+++ facebook-login/src/components/login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./login.css"
 
 function Login() {
     return (
-        <div className='login template d-flex  justify-content-center align-items-center w-100 100-vh bg-primary'>
-            <div className='40-w p-5 rounded '>
+        <div className='login template d-flex  justify-content-center align-items-center vh-100 bg-primary'>
+            <div className='form-container p-5 rounded bg-white  '>
                 <form>
-                    <h3>Sign In</h3>
+                    <h3 className='text-center'>Sign In</h3>
                     <div className='mb-2'>
                         <label htmlFor='email'>Email</label>
                         <input type="email" placeholder=" Enter Email" className="form-control" />
@@ -16,15 +17,15 @@ function Login() {
                     </div>
                     <div className='mb-2'>
                         <input type="checkbox" className='custom-control custom-checkbox' id="check" />
-                        <label htmlFor='check' className='custom-input-label'>
+                        <label htmlFor='check' className='custom-input-label ms-2 '>
                             Remeber me
                         </label>
                     </div>
                     <div className='d-grid'>
                         <button className='btn btn-primary'>Sign In</button>
                     </div>
-                    <p className='text-right'> 
-                        Forgot <a href=''>Password?</a><a href=''>Sign up</a>
+                    <p className='text-end mt-2 '> 
+                        Forgot <a href=''>Password?</a><a href='' className='ms-2' >Sign up</a>
                     </p>
                 </form>
             </div>
